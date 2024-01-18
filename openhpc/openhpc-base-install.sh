5530042446e7cbe22ab6c7351a874f897a5d80ad
Mike Renfro <mike+github@renf.ro>
2024-01-18 15:17:57 -0600

    verbosify ohpc installation

--- openhpc/openhpc-base-install.sh
+++ openhpc/openhpc-base-install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-YUM="yum -q -y"
+YUM="yum -y"
 sms_ip="$(nmcli device show eth1 | grep IP4.ADDRESS | awk '{print $NF}' | cut -d/ -f1)"
 sed -ie "s/127.0.1.1/${sms_ip}/" /etc/hosts
 echo "Yum updates"
-${YUM} update --exclude='kernel*'
+${YUM} update # --exclude='kernel*'
 echo "OHPC repo"
 ${YUM} install http://repos.openhpc.community/OpenHPC/2/EL_8/x86_64/ohpc-release-2-1.el8.x86_64.rpm
 ${YUM} install dnf-plugins-core
